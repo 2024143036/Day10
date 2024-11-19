5fbf5326f6a778ecc51ec682ee1eea868c97b889
2024143036 <2024143036@g.shingu.ac.kr>
2024-11-19 11:18:00 +0900

    Add files via upload

---
+++ 2024143036/day10/src/day10/Array1.java
@@ -0,0 +1,29 @@
+package day10;
+
+public class Array1 {
+
+	public static void main(String[] args) {
+			double[] arrd = new double[5];
+			arrd[0] = 3.12;
+			arrd[1] = 1.23;
+			String[] grade = {"kor", "math", "java"};
+			char c[] = { 'n', 'h' };
+			
+			int arr[] = {1,2,3};
+			int i;
+			for(i = 0; i < arr.length; i++) {
+				System.out.print(arr[i] + " ");
+			}
+			
+			arr[2] = 5;
+			System.out.println(" ");
+			for( i = 0; i < arr.length; i++) {
+				System.out.print(arr[i] + " ");
+			}
+			
+			System.out.println(" ");
+			for( i = 0; i < arr.length; i++) {
+				System.out.print(arr[i]+ " ");
+			}
+	}
+}
